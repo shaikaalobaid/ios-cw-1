fd28936515f3d55e4ca303f2e4f3edf59e105c3f
shaikaalobaid <salobaid250@gmail.com>
2022-03-02 19:51:03 +0300

    done

---
+++ cw2 day1/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Shaika alobaid on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello I am shaika")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cw2 day1/Shared/cw2_day1App.swift
@@ -0,0 +1,17 @@
+//
+//  cw2_day1App.swift
+//  Shared
+//
+//  Created by Shaika alobaid on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw2_day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
